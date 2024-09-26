6e8eb0486bf52cc3bda9a029a4eb2169692e84b0
mmmanuelO-O <joseloarce2004@gmail.com>
2024-09-25 23:06:53 -0600

    Trabajo Aplicado II
    Archivo de R para la Actividad Evaluada II, será luego HTLM y R Markdown

---
+++ TA II.R
@@ -0,0 +1,6 @@
+#Ejercicios
+
+#El Instituto Nacional de Estadística y Censos (INEC, 2020) estimó que el ingresom
+per cápita promedio de Costa Rica en julio de 2020 es 326483 colones, con un
+margen de error de ±11423 colones calculado al 95 % de confianza. Construya el
+intervalo de confianza para el promedio e interprételo.
